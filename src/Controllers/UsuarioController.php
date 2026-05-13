84e96464afcfa4ba36e8fa52df1b6134fba2ff7c
emi <miusdiaries@gmail.com>
2026-05-12 19:40:54 -0500

    Controlador y autoload funcionando

---
+++ src/Controllers/UsuarioController.php
@@ -0,0 +1,8 @@
+<?php
+namespace App\Controllers;
+
+class UsuarioController {
+    public function saludar() {
+        return "¡Hola! El controlador está funcionando correctamente siguiendo PSR-4.";
+    }
+}
